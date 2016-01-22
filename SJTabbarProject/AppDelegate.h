b25604a3c9b3b041ec530e3c64f14177cb7687db
fushengjun <fushengjun@xiangshang360.com>
2016-01-22 16:40:09 +0800

    Initial Commit

---
+++ SJTabbarProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJTabbarProject
+//
+//  Created by SPIREJ on 16/1/22.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJTabbarProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJTabbarProject
+//
+//  Created by SPIREJ on 16/1/22.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
